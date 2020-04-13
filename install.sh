0c331b39dffb3d02b53907cc73b169a348252012
e <ern4273@gmail.com>
2020-04-13 20:21:10 +0900

    Add zsh settings

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# add submodule
+git submodule update --init --recursive
+
+# prezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+ ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# symlink dotfiles
+ln -sf ~/dotfiles/.zprezto ~/.zprezto
+ln -sf ~/dotfiles/nvim/dein ~/.dein
+ln -sf ~/dotfiles/nvim/dein.toml ~/.dein.toml
+ln -sf ~/dotfiles/nvim/init.vim ~/.init.vim
+ln -sf ~/dotfiles/zsh/zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/zsh/zprofile ~/.zprofile
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+
+# change shell
+chsh -s $(which zsh)
+
+source ~/dotfiles/.zshrc
+source ~/dotfiles/.zpreztorc
