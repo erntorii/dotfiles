b8ee2d6bd61058421ab70cc4a0d4149fffe1e6ab
erntorii <erntorii@gmail.com>
2021-09-29 17:27:30 +0900

    移行のための設定調整

--- install.sh
+++ install.sh
@@ -9,13 +9,8 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-# symlink dotfiles
-ln -sf ~/dotfiles/nvim/dein ~/.dein
-ln -sf ~/dotfiles/nvim/dein.toml ~/.dein.toml
+# symlink
 ln -sf ~/dotfiles/nvim/init.vim ~/.init.vim
-ln -sf ~/dotfiles/zsh/.zprezto ~/.zprezto
-ln -sf ~/dotfiles/zsh/zpreztorc ~/.zpreztorc
-ln -sf ~/dotfiles/zsh/zprofile ~/.zprofile
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 
 # change shell
