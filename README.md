da4317adc5f95d7f66782b1a213aa43e12d1382b
erntorii <erntorii@gmail.com>
2021-09-30 11:44:11 +0900

    fix

---
+++ README.md
@@ -0,0 +1,5 @@
+```zsh
+cd dotfiles
+chmod 744 ./install.sh
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -10,11 +10,17 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 
 # symlink
-ln -sf ~/dotfiles/nvim/init.vim ~/.init.vim
+ln -sf ~/dotfiles/zsh/zprezto ~/.zprezto
+ln -sf ~/dotfiles/zsh/zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/nvim/init.vim ~/.init.vim
 
 # change shell
 chsh -s $(which zsh)
 
-source ~/dotfiles/.zshrc
-source ~/dotfiles/.zpreztorc
+source ~/.zpreztorc
+source ~/.zshrc
+
+# vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
